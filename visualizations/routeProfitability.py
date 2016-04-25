a63610343374427b3622f6dd977539c6a6b39fd0
James Blackwood <jamesblackwood@caltech.edu>
2016-04-25 05:37:06 +0000

    Added rider payment breakdown

--- visualizations/routeProfitability.py
+++ visualizations/routeProfitability.py
@@ -1,7 +1,9 @@
 import numpy as np 
-import matplotlib.pyplot as plt 
+import matplotlib
+matplotlib.use('Agg')
 import pandas as pd 
 import os 
+import matplotlib.pyplot as plt
 
 def getFiles(PATH):
 
@@ -19,21 +21,25 @@ def getDataFrame(path, files):
 	for file in files:
 		frame = pd.DataFrame.from_csv(path + file, index_col=False, infer_datetime_format=True)
 		frames.append(frame)
-
 	return pd.concat(frames)  
 
 
 def aggregateRouteCost(frame):
 	route_costs = frame.groupby('Route').agg({'TotalCost': np.sum})
-	
-	route_costs.plot(kind='bar',color='r', legend=False)
+	plot = route_costs.plot(kind='bar',color='r', legend=False)
+	fig1 = plot.get_figure()
 	plt.xlabel('Route')
 	plt.ylabel('Revenue ($)')
 	plt.title('Total Revenue of Routes')
-	plt.show()
-
-
-
+	fig1.savefig('vizzy1.jpg')
+	
+	plot2 = frame.TransactionType.value_counts().plot(kind='bar')
+	fig2 = plot2.get_figure()
+	plt.xlabel('Transaction Type')
+	plt.ylabel('Number of instances')
+	plt.title('Transaction Methods')
+	fig2.savefig('vizzy2.jpg')
+    
 
 if __name__ == "__main__":
 
