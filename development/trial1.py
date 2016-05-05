246cc9f329ad5573ca171abe76b34e47926ea107
James Blackwood <jamesblackwood@caltech.edu>
2016-05-05 06:04:45 +0000

    Added regression and new preprocessed data

--- development/trial1.py
+++ development/trial1.py
@@ -5,8 +5,7 @@ import os
 import matplotlib.pyplot as plt
 import sklearn
 from pandas.stats.api import ols
-import statsmodels.api as sm
-from statsmodels.iolib.summary2 import summary_col
+
 
 def getDataFrame(path, filename):
     os.chdir(path)
@@ -17,10 +16,5 @@ def getDataFrame(path, filename):
 if __name__ == "__main__":
 	PATH = '../data/mergedData/'
 	df = getDataFrame(PATH, "For_Jim.csv")
-	res = sm.OLS(y=df['Avg Arrival Diff'], x=df[['Date','20cc','51','52','60','32','31','20cw','10','40']], intercept = True)
+	res = ols(y=df['Avg Arrival Diff'], x=df[['Date','20cc','51','52','60','32','31','20cw','10','40']], intercept = True)
 	print(res)
-	print summary_col(res,stars=True,float_format='%0.2f',
-                  model_names=['Linear Model 1'],
-                  info_dict={'N':lambda x: "{0:d}".format(int(x.nobs)),
-                             'R2':lambda x: "{:.2f}".format(x.rsquared)})
-
